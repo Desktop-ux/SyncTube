3a0cec8f7a6f132e3a99f21759c5885a54c6b4bb
Ayaan <desktop.ux@gmail.com>
2026-03-17 23:49:18 +0530

    video player updated

--- socketHandler.js
+++ socketHandler.js
@@ -1,61 +1,55 @@
-const roomManager = require("./roomManager")
-const Room = require("./models/Room")
+const roomManager = require("./roomManager");
+const Room = require("./models/Room");
 
 function canControl(roomId, userId) {
+  const role = roomManager.getUserRole(roomId, userId);
 
-  const role = roomManager.getUserRole(roomId, userId)
-
-  return role === "host" || role === "moderator"
+  return role === "host" || role === "moderator";
 }
 
 function socketHandler(io) {
-
   io.on("connection", (socket) => {
-
-    console.log("User connected:", socket.id)
+    console.log("User connected:", socket.id);
 
     socket.on("join_room", async ({ roomId, username }) => {
+      console.log("join_room event:", roomId, username);
 
-      console.log("join_room event:", roomId, username)
+      let room = roomManager.getRoom(roomId);
+      let dbRoom = await Room.findOne({ roomId });
 
-      let room = roomManager.getRoom(roomId)
-
-      let role = "participant"
+      let role = "participant";
 
       if (!room) {
-
-        role = "host"
+        role = "host";
 
         room = roomManager.createRoom(roomId, {
           id: socket.id,
           username,
-          role
-        })
+          role,
+        });
 
-        await Room.create({
-          roomId,
-          host: username,
-          videoId: null,
-          playState: "pause",
-          currentTime: 0,
-          participants: [
-            {
-              userId: socket.id,
-              username,
-              role
-            }
-          ]
-        })
-
-        console.log("Creating new room:", roomId)
-
+        if (!dbRoom) {
+          dbRoom = await Room.create({
+            roomId,
+            host: username,
+            videoId: null,
+            playState: "pause",
+            currentTime: 0,
+            participants: [
+              {
+                userId: socket.id,
+                username,
+                role,
+              },
+            ],
+          });
+        }
       } else {
-
         roomManager.joinRoom(roomId, {
           id: socket.id,
           username,
-          role
-        })
+          role,
+        });
 
         await Room.updateOne(
           { roomId },
@@ -64,227 +58,189 @@ function socketHandler(io) {
               participants: {
                 userId: socket.id,
                 username,
-                role
-              }
-            }
-          }
-        )
-
-        console.log("Joining existing room:", roomId)
-
+                role,
+              },
+            },
+          },
+        );
       }
 
-      socket.join(roomId)
+      socket.join(roomId);
 
-      const updatedRoom = roomManager.getRoom(roomId)
+      const updatedRoom = roomManager.getRoom(roomId);
 
-      io.to(roomId).emit("participants_update", updatedRoom.participants)
+      io.to(roomId).emit("participants_update", updatedRoom.participants);
+
+      const roomState = await Room.findOne({ roomId });
 
       socket.emit("sync_state", {
-        videoId: updatedRoom.videoId,
-        playState: updatedRoom.playState,
-        currentTime: updatedRoom.currentTime
-      })
-
-    })
-
+        videoId: roomState.videoId,
+        playState: roomState.playState,
+        currentTime: roomState.currentTime,
+      });
+    });
 
     socket.on("play", async ({ roomId }) => {
-
       if (!canControl(roomId, socket.id)) {
-
         socket.emit("permission_denied", {
           action: "play",
-          message: "You do not have permission to control playback"
-        })
+          message: "You do not have permission to control playback",
+        });
 
-        return
+        return;
       }
 
       roomManager.updateVideoState(roomId, {
-        playState: "play"
-      })
+        playState: "play",
+      });
 
-      await Room.updateOne(
-        { roomId },
-        { playState: "play" }
-      )
+      await Room.updateOne({ roomId }, { playState: "play" });
 
-      socket.to(roomId).emit("play")
+      socket.to(roomId).emit("play");
 
-      console.log("play event:", roomId)
-
-    })
-
+      console.log("play event:", roomId);
+    });
 
     socket.on("pause", async ({ roomId }) => {
-
       if (!canControl(roomId, socket.id)) {
-
         socket.emit("permission_denied", {
           action: "pause",
-          message: "You do not have permission to control playback"
-        })
+          message: "You do not have permission to control playback",
+        });
 
-        return
+        return;
       }
 
       roomManager.updateVideoState(roomId, {
-        playState: "pause"
-      })
+        playState: "pause",
+      });
 
-      await Room.updateOne(
-        { roomId },
-        { playState: "pause" }
-      )
+      await Room.updateOne({ roomId }, { playState: "pause" });
 
-      socket.to(roomId).emit("pause")
+      socket.to(roomId).emit("pause");
 
-      console.log("pause event:", roomId)
-
-    })
-
+      console.log("pause event:", roomId);
+    });
 
     socket.on("seek", async ({ roomId, time }) => {
-
       if (!canControl(roomId, socket.id)) {
-
         socket.emit("permission_denied", {
           action: "seek",
-          message: "You do not have permission to control playback"
-        })
+          message: "You do not have permission to control playback",
+        });
 
-        return
+        return;
       }
 
       roomManager.updateVideoState(roomId, {
-        currentTime: time
-      })
+        currentTime: time,
+      });
 
-      await Room.updateOne(
-        { roomId },
-        { currentTime: time }
-      )
+      await Room.updateOne({ roomId }, { currentTime: time });
 
-      socket.to(roomId).emit("seek", { time })
+      socket.to(roomId).emit("seek", { time });
 
-      console.log("seek event:", roomId, time)
-
-    })
-
+      console.log("seek event:", roomId, time);
+    });
 
     socket.on("change_video", async ({ roomId, videoId }) => {
-
       if (!canControl(roomId, socket.id)) {
-
         socket.emit("permission_denied", {
           action: "change_video",
-          message: "You do not have permission to control playback"
-        })
+          message: "You do not have permission to control playback",
+        });
 
-        return
+        return;
       }
 
       roomManager.updateVideoState(roomId, {
         videoId,
-        currentTime: 0
-      })
+        currentTime: 0,
+      });
 
       await Room.updateOne(
         { roomId },
         {
           videoId,
-          currentTime: 0
-        }
-      )
+          currentTime: 0,
+        },
+      );
 
-      io.to(roomId).emit("change_video", { videoId })
+      io.to(roomId).emit("change_video", { videoId });
 
-      console.log("change_video event:", roomId, videoId)
-
-    })
-
+      console.log("change_video event:", roomId, videoId);
+    });
 
     socket.on("assign_role", ({ roomId, userId, role }) => {
-
-      const senderRole = roomManager.getUserRole(roomId, socket.id)
+      const senderRole = roomManager.getUserRole(roomId, socket.id);
 
       if (senderRole !== "host") {
-
-        console.log("Only host can assign roles")
-        return
+        console.log("Only host can assign roles");
+        return;
       }
 
-      const updatedRoom = roomManager.assignRole(roomId, userId, role)
+      const updatedRoom = roomManager.assignRole(roomId, userId, role);
 
-      if (!updatedRoom) return
+      if (!updatedRoom) return;
 
-      io.to(roomId).emit("participants_update", updatedRoom.participants)
+      io.to(roomId).emit("participants_update", updatedRoom.participants);
 
-      console.log("Role assigned:", userId, role)
-
-    })
-
+      console.log("Role assigned:", userId, role);
+    });
 
     socket.on("remove_participant", async ({ roomId, userId }) => {
-
-      const senderRole = roomManager.getUserRole(roomId, socket.id)
+      const senderRole = roomManager.getUserRole(roomId, socket.id);
 
       if (senderRole !== "host") {
-
-        console.log("Only host can remove participants")
-        return
+        console.log("Only host can remove participants");
+        return;
       }
 
-      const updatedRoom = roomManager.removeParticipant(roomId, userId)
+      const updatedRoom = roomManager.removeParticipant(roomId, userId);
 
-      if (!updatedRoom) return
+      if (!updatedRoom) return;
 
       await Room.updateOne(
         { roomId },
         {
           $pull: {
-            participants: { userId }
-          }
-        }
-      )
+            participants: { userId },
+          },
+        },
+      );
 
-      io.to(roomId).emit("participants_update", updatedRoom.participants)
+      io.to(roomId).emit("participants_update", updatedRoom.participants);
 
-      io.to(userId).emit("removed_from_room")
+      io.to(userId).emit("removed_from_room");
 
-      console.log("User removed:", userId)
+      console.log("User removed:", userId);
+    });
 
-    })
-
+    
 
     socket.on("disconnect", async () => {
+      const roomId = roomManager.removeUser(socket.id);
 
-      const roomId = roomManager.removeUser(socket.id)
-
-      if (!roomId) return
+      if (!roomId) return;
 
       await Room.updateOne(
         { roomId },
         {
           $pull: {
-            participants: { userId: socket.id }
-          }
-        }
-      )
+            participants: { userId: socket.id },
+          },
+        },
+      );
 
-      const room = roomManager.getRoom(roomId)
+      const room = roomManager.getRoom(roomId);
 
       if (room) {
-        io.to(roomId).emit("participants_update", room.participants)
+        io.to(roomId).emit("participants_update", room.participants);
       }
 
-      console.log("User disconnected:", socket.id)
-
-    })
-
-  })
-
+      console.log("User disconnected:", socket.id);
+    });
+  });
 }
 
-module.exports = socketHandler
+module.exports = socketHandler;
